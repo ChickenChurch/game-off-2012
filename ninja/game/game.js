7eb0107b22d17547a98f16f53d5cd78051886538
Shawn Daichendt <shawn.daichendt@gmail.com>
2012-11-25 15:24:05 -0500

    updated the gameplay

--- ninja/game/game.js
+++ ninja/game/game.js
@@ -13,7 +13,25 @@ goog.require('lime.animation.MoveBy');
 goog.require('lime.animation.RotateBy');
 goog.require('lime.animation.Loop');
 
-// initialize variables
+// declare variables
+var director;
+var player;
+var playerSS;
+var gameScene;
+var mainLayer;
+var enemies;
+var enemy_create_time;
+var background;
+var life_label;
+var life_1;
+var life_2;
+var life_3;
+var score_title_label;
+var score_label;
+
+var life_count = 4;
+var score = 0;
+
 var screen_width = 1024;
 var screen_height = 700;
 
@@ -23,32 +41,69 @@ var player_left = 1;
 var player_right = 3;
 var player_up = 4;
 
-var push_left, push_right, push_up, push_down = false;
+var push_left = false;
+var push_right = false;
+var push_up = false;
+var push_down = false;
 
+var hit_padding = -90;
+
+var enemy_normal_speed = 200;
+var enemy_push_speed = 500;
 var enemy_create_interval = 100;
+var enemy_rotation = 720;
 
 // entrypoint
 game.start = function(){
 	// initialize variables
-	var director = new lime.Director(document.body,screen_width, screen_height);
-	var playerSS = new lime.SpriteSheet('assets/player.png',lime.ASSETS.player.json,lime.parser.JSON);
-	var scene = new lime.Scene();
-	var layer = new lime.Layer().setPosition(screen_width/2, screen_height/2).setRenderer(lime.Renderer.CANVAS);	
-	var enemies = new Array();
-	var enemy_create_time = enemy_create_interval;
+	director = new lime.Director(document.body,screen_width, screen_height);
+	playerSS = new lime.SpriteSheet('assets/player.png',lime.ASSETS.player.json,lime.parser.JSON);
+	gameScene = new lime.Scene();
+	mainLayer = new lime.Layer().setPosition(screen_width/2, screen_height/2).setRenderer(lime.Renderer.CANVAS);	
+	enemies = new Array();
+	enemy_create_time = enemy_create_interval;
 	
 	// setup background
-	var background = new lime.Sprite().setFill('assets/background.png');
-    layer.appendChild(background);
+	background = new lime.Sprite().setFill('assets/background.png');
+    mainLayer.appendChild(background);
 	
-	// create the character sprite
-	var player = new lime.Sprite().setPosition(0,50);
+	// setup the character sprite
+	player = new lime.Sprite().setPosition(0,50);
 	player.setFill(playerSS.getFrame(player_normal));
-	layer.appendChild(player);
-
-	// set current scene active
-	scene.appendChild(layer);
-	director.replaceScene(scene);
+	mainLayer.appendChild(player);
+	
+	// setup life bar
+	life_label = new lime.Label().setText('LIFE')
+		.setFontFamily('Comic Sans MS').setFontColor('#fff').setFontWeight('bold')
+		.setFontSize(40)
+		.setPosition(-(screen_width/2 - 70), -(screen_height/2 - 25));
+    mainLayer.appendChild(life_label);
+	
+	life_1 = new lime.Sprite().setFill('assets/life_full.png')
+		.setPosition(-(screen_width/2 - 40), -(screen_height/2 - 80));
+	mainLayer.appendChild(life_1);
+	
+	life_2 = new lime.Sprite().setFill('assets/life_full.png')
+		.setPosition(-(screen_width/2 - 90), -(screen_height/2 - 80));
+	mainLayer.appendChild(life_2);
+	
+	life_3 = new lime.Sprite().setFill('assets/life_full.png')
+		.setPosition(-(screen_width/2 - 140), -(screen_height/2 - 80));
+	mainLayer.appendChild(life_3);
+	
+	// setup score bar
+	score_title_label = new lime.Label().setText('SCORE')
+		.setFontFamily('Comic Sans MS').setFontColor('#fff').setFontWeight('bold')
+		.setFontSize(40)
+		.setPosition((screen_width/2 - 90), -(screen_height/2 - 25));
+    mainLayer.appendChild(score_title_label);
+    
+	score_label = new lime.Label().setText(score)
+		.setFontFamily('Comic Sans MS').setFontColor('#fff').setFontWeight('bold')
+		.setFontSize(40)
+		.setAnchorPoint(1,1)
+		.setPosition((screen_width/2 - 25), -(screen_height/2 - 95));
+    mainLayer.appendChild(score_label);
 	
 	// pressing 'left' action
 	KeyboardJS.on('a', 
@@ -98,8 +153,8 @@ game.start = function(){
 		}
 	);
 	
-	// pressing 'down' action
-	KeyboardJS.on('s', 
+	// pressing 'space' action
+	KeyboardJS.on('space', 
 		function(){
 			if (push_down == false) {
 				push_down = true;
@@ -126,21 +181,21 @@ game.start = function(){
 				var new_enemy = new lime.Sprite().setPosition(screen_width/2,50);
 				new_enemy.setFill('assets/shiruken.png');
 				new_enemy.direction = 'left';
-				layer.appendChild(new_enemy);
+				mainLayer.appendChild(new_enemy);
 			} else if (starting_position == 1) { // right
 				new_enemy = new lime.Sprite().setPosition(-(screen_width/2),50);
 				new_enemy.setFill('assets/shiruken.png');
 				new_enemy.direction = 'right';
-				layer.appendChild(new_enemy);
+				mainLayer.appendChild(new_enemy);
 			} else if (starting_position == 2) { // down
 				new_enemy = new lime.Sprite().setPosition(0,-(screen_height/2));
 				new_enemy.setFill('assets/shiruken.png');
 				new_enemy.direction = 'down';
-				layer.appendChild(new_enemy);
+				mainLayer.appendChild(new_enemy);
 			}
 			
 			// rotate the enemy
-			var spin = new lime.animation.Loop(new lime.animation.RotateBy(720));
+			var spin = new lime.animation.Loop(new lime.animation.RotateBy(enemy_rotation));
 			spin.setEasing(lime.animation.Easing.LINEAR);
 			new_enemy.runAction(spin);
 			
@@ -148,39 +203,66 @@ game.start = function(){
 			enemies.push(new_enemy);
 		}
 	
+		// checks for every active enemy
 		for (var i = 0; i < enemies.length; i++) {
 			var enemy = enemies[i];
+			
+			// destroy the enemy if it is touching the player
+			if (goog.math.Box.intersectsWithPadding(player.getBoundingBox(),enemy.getBoundingBox(), hit_padding)) {
+				mainLayer.removeChild(enemy);
+				enemies.splice(i, 1);
+				
+				// the player loses a life
+				life_count -= 1;
+				if (life_count == 0) {
+					// GAME OVER
+					return;
+				} else if (life_count == 3) {
+					life_3.setFill('assets/life_empty.png');
+				} else if (life_count == 2) {
+					life_2.setFill('assets/life_empty.png');
+				} else {
+					life_1.setFill('assets/life_empty.png');
+				}
+				return;
+			}
 		
 			// destroy the enemy if it out of the bound of the view
 			var x = Math.abs(enemy.getPosition().x);
-			console.log(x);
 			var y = Math.abs(enemy.getPosition().y);
-			console.log(y);
 			
 			if (x > screen_width/2 || y > screen_height/2) {
-				layer.removeChild(enemy);
+				mainLayer.removeChild(enemy);
 				enemies.splice(i, 1);
+				
+				// increase the player's score
+				score += 1;
+				//console.log(score);
+				
+				// update the score label
+				score_label.setText(score);
+				
 				return;
 			}
 			
 			// push the enemy in the opposite direction if the player is pushing it
 			var move;
 			if (enemy.direction == 'down' && push_up == true) {
-				move = new lime.animation.MoveBy(0,-500);
+				move = new lime.animation.MoveBy(0,-(enemy_push_speed));
 			} else if (enemy.direction == 'left' && push_right == true) {
-				move = new lime.animation.MoveBy(500,0);;
+				move = new lime.animation.MoveBy(enemy_push_speed,0);
 			} else if (enemy.direction == 'right' && push_left == true) {
-				move = new lime.animation.MoveBy(-500,0);
+				move = new lime.animation.MoveBy(-(enemy_push_speed),0);
 			} else {
 				// move the enemy forward along its directions
 				if (enemy.direction == 'left'){
-					move = new lime.animation.MoveBy(-200,0);
+					move = new lime.animation.MoveBy(-(enemy_normal_speed),0);
 				} else if (enemy.direction == 'right'){
-					move = new lime.animation.MoveBy(200, 0);
+					move = new lime.animation.MoveBy(enemy_normal_speed, 0);
 				} else if (enemy.direction == 'up'){
-					move = new lime.animation.MoveBy(0,-200);
+					move = new lime.animation.MoveBy(0,-(enemy_normal_speed));
 				} else if (enemy.direction == 'down'){
-					move = new lime.animation.MoveBy(0,200);
+					move = new lime.animation.MoveBy(0,enemy_normal_speed);
 				}
 			}
 			
@@ -189,6 +271,10 @@ game.start = function(){
 			enemy.runAction(move);
 		}
 	});
+	
+	// make the game scene active
+	gameScene.appendChild(mainLayer);
+	director.replaceScene(gameScene);
 }
 
 
