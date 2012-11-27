8c182c2490ee6e15a95df31874fd6b4291ae2fd9
Shawn Daichendt <shawn.daichendt@gmail.com>
2012-11-26 21:28:52 -0500

    some more game twetweaks

--- ninja/game/game.js
+++ ninja/game/game.js
@@ -56,17 +56,281 @@ var hit_padding = -90;
 
 var pull_speed = 5;
 
-var enemy_normal_speed = 500;
+var enemy_start_speed = 300;
 var enemy_push_speed = 500;
 var enemy_create_interval = 100;
 var enemy_rotation = 720;
+var enemy_kill_count = 0;
+var enemy_speed = 0;
 
 var extra_life_container_diameter = 800;
 var extra_life_rotation_speed = 90;
 var extra_life_create_interval = 500;
 var extra_life_create_timer = 0;
 
-// entrypoint
+/*
+ *
+ * Keyboard Controls
+ *
+ */
+
+// A - push left
+
+KeyboardJS.on('a', 
+	function(){
+		if (push_left == false) {
+			push_left = true;
+			player.setFill(playerSS.getFrame(player_left));	
+		}
+	}, 
+	function(){
+		push_left = false;
+		if (KeyboardJS.activeKeys().length == 0) {
+			player.setFill(playerSS.getFrame(player_normal));
+		}
+	}
+);
+	
+// D - push right
+
+KeyboardJS.on('d', 
+	function(){
+		if (push_right == false) {
+			push_right = true;
+			player.setFill(playerSS.getFrame(player_right));
+		}
+	}, 
+	function(){
+		push_right = false;
+		if (KeyboardJS.activeKeys().length == 0) {
+			player.setFill(playerSS.getFrame(player_normal));
+		}
+	}
+);
+	
+// W - push up
+KeyboardJS.on('w', 
+	function(){
+		if (push_up == false) {
+			push_up = true;
+			player.setFill(playerSS.getFrame(player_up));	
+		}
+	}, 
+	function(){
+		push_up = false;
+		if (KeyboardJS.activeKeys().length == 0) {
+			player.setFill(playerSS.getFrame(player_normal));
+		}
+	}
+);
+	
+// SPACE - pull
+
+KeyboardJS.on('space', 
+	function(){
+		if (push_down == false) {
+			push_down = true;
+			player.setFill(playerSS.getFrame(player_down));	
+		}
+	}, 
+	function(){
+		push_down = false;
+		if (KeyboardJS.activeKeys().length == 0) {
+			player.setFill(playerSS.getFrame(player_normal));
+		}
+	}
+);
+
+// ENTER - restart the game
+/*
+KeyboardJS.on('enter', 
+	function(){
+		// pause the game
+		game.pause();
+	}
+);
+*/
+
+game.pause = function(){
+	lime.scheduleManager.unschedule(game.run);
+};
+
+game.over = function(){
+	console.log('game over!');
+	game.director.replaceScene(gameOverScene);
+	lime.scheduleManager.unschedule(game.run);
+}
+
+game.run = function(){
+	// check to see if a new extra life object should be created
+	if (extra_life == null) {
+		if (extra_life_create_timer < extra_life_create_interval) {
+			extra_life_create_timer++;
+		} else {
+			extra_life_container = new lime.Sprite().setSize(extra_life_container_diameter, extra_life_container_diameter);
+			mainLayer.appendChild(extra_life_container);
+	
+			extra_life = new lime.Sprite().setFill('assets/life.png').setPosition(0, (extra_life_container.getSize().height)/2);
+			extra_life_container.appendChild(extra_life);
+				
+			// reset the extra life create timer
+			extra_life_create_timer = 0;
+		}	
+	} else {
+		// spin the life object
+		var rotate = new lime.animation.RotateBy(extra_life_rotation_speed);
+		rotate.setEasing(lime.animation.Easing.LINEAR);
+		extra_life_container.runAction(rotate);
+		
+		// shrink the life object's rotation radius is the player is pulling it
+		if (push_down == true) {
+			extra_life.setPosition(extra_life.getPosition().x, extra_life.getPosition().y - pull_speed);
+		}
+		
+		// if the life object makes contact with the player, destroy it
+		if (goog.math.Box.intersectsWithPadding(player.getBoundingBox(),extra_life.getBoundingBox(), hit_padding)) {
+			mainLayer.removeChild(extra_life_container);
+			extra_life_container = null;
+			extra_life = null;
+				
+		// the player gains a life (if they already aren't at max life)
+			if (life_count < 4) {
+				life_count += 1;
+			}
+				
+			if (life_count > 1) {
+				life_1.setFill('assets/life_full.png');
+			}
+			if (life_count > 2) {
+				life_2.setFill('assets/life_full.png');
+			}
+			if (life_count > 3) {
+				life_3.setFill('assets/life_full.png');
+			}
+				
+			// increase the player's score
+			score += extra_life_score_amount;
+				
+			// update the score label
+			score_label.setText(score);
+		}
+	} 
+		
+	// check to see if a new emery should be created
+	enemy_create_time++;
+	if (enemy_create_time > enemy_create_interval) {
+		enemy_create_time = 0;
+			
+		// create a new enemy at one of four random positions (left, right, up, down)
+		var starting_position = parseInt(Math.random() * 3);
+		if (starting_position == 0) { // left
+			var new_enemy = new lime.Sprite().setPosition(screen_width/2,50);
+			new_enemy.setFill('assets/shiruken.png');
+			new_enemy.direction = 'left';
+			mainLayer.appendChild(new_enemy);
+		} else if (starting_position == 1) { // right
+			new_enemy = new lime.Sprite().setPosition(-(screen_width/2),50);
+			new_enemy.setFill('assets/shiruken.png');
+			new_enemy.direction = 'right';
+			mainLayer.appendChild(new_enemy);
+		} else if (starting_position == 2) { // down
+			new_enemy = new lime.Sprite().setPosition(0,-(screen_height/2));
+			new_enemy.setFill('assets/shiruken.png');
+			new_enemy.direction = 'down';
+			mainLayer.appendChild(new_enemy);
+		}
+			
+		// push enemy to enemies array
+		enemies.push(new_enemy);
+	}
+	
+	// checks for every active enemy
+	for (var i = 0; i < enemies.length; i++) {
+		var enemy = enemies[i];
+			
+		// destroy the enemy if it is touching the player
+		if (goog.math.Box.intersectsWithPadding(player.getBoundingBox(),enemy.getBoundingBox(), hit_padding)) {
+			mainLayer.removeChild(enemy);
+			enemies.splice(i, 1);
+				
+			// the player loses a life
+			life_count -= 1;
+			if (life_count == 0) {
+				// GAME OVER
+				game.over();
+				return;
+			} else if (life_count == 3) {
+				life_3.setFill('assets/life_empty.png');
+			} else if (life_count == 2) {
+				life_2.setFill('assets/life_empty.png');
+			} else {
+				life_1.setFill('assets/life_empty.png');
+			}
+		}
+		
+		// destroy the enemy if it out of the bound of the view
+		var x = Math.abs(enemy.getPosition().x);
+		var y = Math.abs(enemy.getPosition().y);
+			
+		if (x > screen_width/2 || y > screen_height/2) {
+			mainLayer.removeChild(enemy);
+			enemies.splice(i, 1);
+				
+			// increase the player's score
+			score += enemy_score_amount;
+				
+			// update the score label
+			score_label.setText(score);
+			
+			// update the enemy kill count
+			enemy_kill_count++;
+			
+			// every time the player kills a certain number of enemies, update the enemy speed
+			if (enemy_kill_count % 5 == 0) {
+				console.log('increase enemy speed!');
+				enemy_speed += 50;
+			}
+				
+			return;
+		}
+			
+		// push the enemy in the opposite direction if the player is pushing it
+		var move;
+		if (enemy.direction == 'down' && push_up == true) {
+			move = new lime.animation.MoveBy(0,-(enemy_push_speed));
+		} else if (enemy.direction == 'left' && push_right == true) {
+			move = new lime.animation.MoveBy(enemy_push_speed,0);
+		} else if (enemy.direction == 'right' && push_left == true) {
+			move = new lime.animation.MoveBy(-(enemy_push_speed),0);
+		} else {
+			// move the enemy forward along its directions
+			if (enemy.direction == 'left'){
+				move = new lime.animation.MoveBy(-(enemy_speed),0);
+			} else if (enemy.direction == 'right'){
+				move = new lime.animation.MoveBy(enemy_speed, 0);
+			} else if (enemy.direction == 'up'){
+				move = new lime.animation.MoveBy(0,-(enemy_speed));
+			} else if (enemy.direction == 'down'){
+				move = new lime.animation.MoveBy(0,enemy_speed);
+			}
+		}
+			
+		// make the enemy movement linear
+		move.setEasing(lime.animation.Easing.LINEAR);
+		enemy.runAction(move);
+			
+		// spin the enemy
+		var spin = new lime.animation.RotateBy(enemy_rotation);
+		spin.setEasing(lime.animation.Easing.LINEAR);
+		enemy.runAction(spin);
+	}
+};
+
+game.setup = function(){
+	enemy_speed = enemy_start_speed;
+	enemy_kill_count = 0;
+}
+
 game.start = function(){
 	// initialize variables
 	game.director = new lime.Director(document.body,screen_width, screen_height);
@@ -135,233 +399,15 @@ game.start = function(){
 		.setPosition((screen_width/2 - 25), -(screen_height/2 - 95));
     mainLayer.appendChild(score_label);
 	
-	// pressing 'left' action
-	KeyboardJS.on('a', 
-		function(){
-			if (push_left == false) {
-				push_left = true;
-				player.setFill(playerSS.getFrame(player_left));	
-			}
-		}, 
-		function(){
-			push_left = false;
-			if (KeyboardJS.activeKeys().length == 0) {
-				player.setFill(playerSS.getFrame(player_normal));
-			}
-		}
-	);
-	
-	// pressing 'right' action
-	KeyboardJS.on('d', 
-		function(){
-			if (push_right == false) {
-				push_right = true;
-				player.setFill(playerSS.getFrame(player_right));
-			}
-		}, 
-		function(){
-			push_right = false;
-			if (KeyboardJS.activeKeys().length == 0) {
-				player.setFill(playerSS.getFrame(player_normal));
-			}
-		}
-	);
-	
-	// pressing 'up' action
-	KeyboardJS.on('w', 
-		function(){
-			if (push_up == false) {
-				push_up = true;
-				player.setFill(playerSS.getFrame(player_up));	
-			}
-		}, 
-		function(){
-			push_up = false;
-			if (KeyboardJS.activeKeys().length == 0) {
-				player.setFill(playerSS.getFrame(player_normal));
-			}
-		}
-	);
-	
-	// pressing 'space' action
-	KeyboardJS.on('space', 
-		function(){
-			if (push_down == false) {
-				push_down = true;
-				player.setFill(playerSS.getFrame(player_down));	
-			}
-		}, 
-		function(){
-			push_down = false;
-			if (KeyboardJS.activeKeys().length == 0) {
-				player.setFill(playerSS.getFrame(player_normal));
-			}
-		}
-	);
-	
-	var runGame = function(){
-		// check to see if a new extra life object should be created
-		if (extra_life == null) {
-			if (extra_life_create_timer < extra_life_create_interval) {
-				extra_life_create_timer++;
-			} else {
-				extra_life_container = new lime.Sprite().setSize(extra_life_container_diameter, extra_life_container_diameter);
-				mainLayer.appendChild(extra_life_container);
-	
-				extra_life = new lime.Sprite().setFill('assets/life.png').setPosition(0, (extra_life_container.getSize().height)/2);
-				extra_life_container.appendChild(extra_life);
-				
-				// reset the extra life create timer
-				extra_life_create_timer = 0;
-			}	
-		} else {
-			// spin the life object
-			var rotate = new lime.animation.RotateBy(extra_life_rotation_speed);
-			rotate.setEasing(lime.animation.Easing.LINEAR);
-			extra_life_container.runAction(rotate);
-		
-			// shrink the life object's rotation radius is the player is pulling it
-			if (push_down == true) {
-				extra_life.setPosition(extra_life.getPosition().x, extra_life.getPosition().y - pull_speed);
-			}
-		
-			// if the life object makes contact with the player, destroy it
-			if (goog.math.Box.intersectsWithPadding(player.getBoundingBox(),extra_life.getBoundingBox(), hit_padding)) {
-				mainLayer.removeChild(extra_life_container);
-				extra_life_container = null;
-				extra_life = null;
-				
-				// the player gains a life (if they already aren't at max life)
-				if (life_count < 4) {
-					life_count += 1;
-				}
-				
-				if (life_count > 1) {
-					life_1.setFill('assets/life_full.png');
-				}
-				if (life_count > 2) {
-					life_2.setFill('assets/life_full.png');
-				}
-				if (life_count > 3) {
-					life_3.setFill('assets/life_full.png');
-				}
-				
-				// increase the player's score
-				score += extra_life_score_amount;
-				
-				// update the score label
-				score_label.setText(score);
-			}
-		} 
-		
-		// check to see if a new emery should be created
-		enemy_create_time++;
-		if (enemy_create_time > enemy_create_interval) {
-			enemy_create_time = 0;
-			
-			// create a new enemy at one of four random positions (left, right, up, down)
-			var starting_position = parseInt(Math.random() * 3);
-			if (starting_position == 0) { // left
-				var new_enemy = new lime.Sprite().setPosition(screen_width/2,50);
-				new_enemy.setFill('assets/shiruken.png');
-				new_enemy.direction = 'left';
-				mainLayer.appendChild(new_enemy);
-			} else if (starting_position == 1) { // right
-				new_enemy = new lime.Sprite().setPosition(-(screen_width/2),50);
-				new_enemy.setFill('assets/shiruken.png');
-				new_enemy.direction = 'right';
-				mainLayer.appendChild(new_enemy);
-			} else if (starting_position == 2) { // down
-				new_enemy = new lime.Sprite().setPosition(0,-(screen_height/2));
-				new_enemy.setFill('assets/shiruken.png');
-				new_enemy.direction = 'down';
-				mainLayer.appendChild(new_enemy);
-			}
-			
-			// push enemy to enemies array
-			enemies.push(new_enemy);
-		}
-	
-		// checks for every active enemy
-		for (var i = 0; i < enemies.length; i++) {
-			var enemy = enemies[i];
-			
-			// destroy the enemy if it is touching the player
-			if (goog.math.Box.intersectsWithPadding(player.getBoundingBox(),enemy.getBoundingBox(), hit_padding)) {
-				mainLayer.removeChild(enemy);
-				enemies.splice(i, 1);
-				
-				// the player loses a life
-				life_count -= 1;
-				if (life_count == 0) {
-					// GAME OVER
-					game.director.replaceScene(gameOverScene);
-					lime.scheduleManager.unschedule(runGame);
-					break;
-				} else if (life_count == 3) {
-					life_3.setFill('assets/life_empty.png');
-				} else if (life_count == 2) {
-					life_2.setFill('assets/life_empty.png');
-				} else {
-					life_1.setFill('assets/life_empty.png');
-				}
-				return;
-			}
-		
-			// destroy the enemy if it out of the bound of the view
-			var x = Math.abs(enemy.getPosition().x);
-			var y = Math.abs(enemy.getPosition().y);
-			
-			if (x > screen_width/2 || y > screen_height/2) {
-				mainLayer.removeChild(enemy);
-				enemies.splice(i, 1);
-				
-				// increase the player's score
-				score += enemy_score_amount;
-				
-				// update the score label
-				score_label.setText(score);
-				
-				return;
-			}
-			
-			// push the enemy in the opposite direction if the player is pushing it
-			var move;
-			if (enemy.direction == 'down' && push_up == true) {
-				move = new lime.animation.MoveBy(0,-(enemy_push_speed));
-			} else if (enemy.direction == 'left' && push_right == true) {
-				move = new lime.animation.MoveBy(enemy_push_speed,0);
-			} else if (enemy.direction == 'right' && push_left == true) {
-				move = new lime.animation.MoveBy(-(enemy_push_speed),0);
-			} else {
-				// move the enemy forward along its directions
-				if (enemy.direction == 'left'){
-					move = new lime.animation.MoveBy(-(enemy_normal_speed),0);
-				} else if (enemy.direction == 'right'){
-					move = new lime.animation.MoveBy(enemy_normal_speed, 0);
-				} else if (enemy.direction == 'up'){
-					move = new lime.animation.MoveBy(0,-(enemy_normal_speed));
-				} else if (enemy.direction == 'down'){
-					move = new lime.animation.MoveBy(0,enemy_normal_speed);
-				}
-			}
-			
-			// make the enemy movement linear
-			move.setEasing(lime.animation.Easing.LINEAR);
-			enemy.runAction(move);
-			
-			// spin the enemy
-			var spin = new lime.animation.RotateBy(enemy_rotation);
-			spin.setEasing(lime.animation.Easing.LINEAR);
-			enemy.runAction(spin);
-		}
-	};
-	
-	lime.scheduleManager.schedule(runGame);
-	
 	// make the game scene active
 	gameScene.appendChild(mainLayer);
 	game.director.replaceScene(gameScene);
+	
+	// setup the game
+	game.setup();
+	
+	// run the game
+	lime.scheduleManager.schedule(game.run);
 }
 
 
